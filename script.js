7dfb9c1717cf8f49612ce6deda9abfb164b9834f
RajveerMadrid <b2028rmsmadrid@pshs.edu.ph>
2025-02-14 18:18:35 +0800

    commit1

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ function insert(num) {
     textview.value = textview.value+num;
 }
 
+// function to clear the text input
 function clean() {
     textview.value = "";
 }
 
+// function for the backspace button
 function back() {
     const exp = textview.value;
     textview.value = exp.substring(0, exp.length-1);
 }
+
+// function to evaluate and solve the contents of the textview
 function equal() {
     result = eval(textview.value)
     console.log(result)
